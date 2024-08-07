f911a36282011988ff13a5fd88d959e8d000f989
EntrenaDev <manuel.entrena@gmail.com>
2024-08-07 13:13:53 +0200

    feat: :sparkles: Added new script discord

---
+++ discord.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Asegúrate de que ambos argumentos se hayan pasado
+if [ "$#" -ne 2 ]; then
+  echo "Uso: $0 \"mensaje\" \"webhook_url\""
+  exit 1
+fi
+
+MESSAGE=$1
+WEBHOOK_URL=$2
+
+# Enviar la notificación a Discord
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"${MESSAGE}\"}" "${WEBHOOK_URL}"
